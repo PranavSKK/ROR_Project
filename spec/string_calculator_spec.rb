270cae5632a6f12cb315fefa0c4f8aee4b16d8ae
pranav <pranav@saranyu.in>
2024-12-25 12:17:24 +0530

    handle empty string and single number input

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  it "returns 0 for an empty string" do
+    calculator = StringCalculator.new
+    expect(calculator.add("")).to eq(0)
+  end
+
+  it "returns the number itself for a single number" do
+    calculator = StringCalculator.new
+    expect(calculator.add("5")).to eq(5)
+  end
+end
