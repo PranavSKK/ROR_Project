3a8490bd532c1627618ffd253b70fd071743ceef
pranav <pranav@saranyu.in>
2024-12-25 12:23:16 +0530

    handle sum with delimeters and negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,20 @@
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.empty? 
-
-    delimiters = /[\n,]/       
-    numbers.split(delimiters).map(&:to_i).sum
-  end
+    def add(numbers)
+      return 0 if numbers.empty?
+  
+      negatives = []
+      numbers.split(/[\n,;]/).each do |num|
+        num = num.to_i
+        if num < 0
+          negatives << num
+        end
+      end
+      
+      if negatives.any?
+        raise "Negative numbers not allowed: #{negatives.join(', ')}"
+      end
+  
+      numbers.split(/[\n,;]/).map(&:to_i).sum
+    end
 end
+  
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("1\n2,3")).to eq(6)
   end
+
+  it "supports custom delimiters" do
+    calculator = StringCalculator.new
+    expect(calculator.add("//;\n1;2")).to eq(3)
+  end
+
+  it "raises an error for multiple negative numbers" do
+    calculator = StringCalculator.new
+    expect { calculator.add("1,-2,-3,-4") }.to raise_error("Negative numbers not allowed: -2, -3, -4")
+  end
+
 end
